ccee1ca1a8daa4628b095e0252835913176a1fec
Kushagra Kalbhawar <90435588+Kushagra1A@users.noreply.github.com>
2026-03-07 09:20:13 +0530

    Update rag_pipeline.py

---
+++ backend/services/rag_pipeline.py
@@ -0,0 +1,16 @@
+from services.pdf_processor import extract_text_from_pdf, split_text
+from services.vector_store import create_vector_store, search
+
+
+def ingest_pdf(file_path):
+    text = extract_text_from_pdf(file_path)
+
+    chunks = split_text(text)
+
+    create_vector_store(chunks)
+
+
+def ask_question(question):
+    context = search(question)
+
+    return context

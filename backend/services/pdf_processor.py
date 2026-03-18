5eaf0db732cae60b28e2c11f7e84e054bc9cf085
Kushagra Kalbhawar <90435588+Kushagra1A@users.noreply.github.com>
2026-03-18 18:16:20 +0530

    Update pdf_processor.py

--- backend/services/pdf_processor.py
+++ backend/services/pdf_processor.py
@@ -1,5 +1,5 @@
 from pypdf import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 def extract_text_from_pdf(file_path):
